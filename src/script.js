7e527ba10a1db63365fd907ad0b495ec23a5b052
TalesRG <talesrodriguesgoncalves98@gmail.com>
2025-03-01 00:01:05 -0300

    feat: concluindo desafio

--- src/script.js
+++ src/script.js
@@ -1,23 +1,43 @@
-//função de mudar imagem pelo id e pela url
+
 function changeImage(id, url) {
   document.getElementById(id).src = url;
 }
-//função de mudar texto pelo id e pelo texto
+
 function changeText(id, text) {
   document.getElementById(id).innerText = text;
 }
+let currentPokemon = 1;
 
-// Daqui para baixo voce ira escrever
-// o código para resolver o desafio
+async function fetchPokemon(pokemonIdOrName) {
+  try {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonIdOrName}`);
+    const data = await response.json();
+
+    changeText("name", data.name.toUpperCase() + " #" + data.id);
+
+    if (data.sprites.front_default) {
+      changeImage("img_sprite_front_default", data.sprites.front_default);
+    } else {
+      changeImage("img_sprite_front_default", "../assets/missingno.png");
+    }
+
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 function previousPokemon() {
-  alert("Pokemon Anterior");
-  //abra o terminal em inspecionar no chrome para visualizar
-  console.log("Pokemon Anterior");
+  if (currentPokemon > 1) {
+    currentPokemon--;
+  }
+  fetchPokemon(currentPokemon);
 }
 
 function nextPokemon() {
-  alert("Pokemon Seguinte");
-  //abra o terminal em inspecionar no chrome para visualizar
-  console.log("Pokemon Seguinte");
+  currentPokemon++;
+  fetchPokemon(currentPokemon);
 }
+
+window.onload = () => {
+  fetchPokemon(currentPokemon);
+};
